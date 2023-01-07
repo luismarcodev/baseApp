12156781bbce3daf4d7fd6b0a7661d6994906612
luis <luismarco.web@gmail.com>
2023-01-07 14:18:49 +0100

    Add home tabBar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,3 @@
-
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
@@ -7,12 +6,52 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(child: Text('Español'),onPressed: (){
-        context.setLocale(Locale('en', ''));
-      },),
-      appBar: AppBar(),
-      body: Container(child: const Text('common.hello').tr(),));
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: SafeArea(
+          child: Scaffold(
+            resizeToAvoidBottomInset: true,
+            appBar: AppBar(
+              //  bottom:
+              title: const Text('common.title').tr(),
+            ),
+            body: Stack(children: [
+              TabBarView(
+                children: [
+                  Icon(Icons.directions_car),
+                  Icon(Icons.directions_transit),
+                  Icon(Icons.directions_bike),
+                ],
+              ),
+              Positioned(
+                bottom: 0,
+                child: Expanded(
+                  child: TabBar(
+                    tabs: [
+                      Tab(
+                          icon: Icon(
+                        Icons.directions_car,
+                        color: Colors.red,
+                      )),
+                      Tab(
+                          icon: Icon(
+                        Icons.directions_transit,
+                        color: Colors.red,
+                      )),
+                      Tab(
+                          icon: Icon(
+                        Icons.directions_bike,
+                        color: Colors.red,
+                      )),
+                    ],
+                  ),
+                ),
+              )
+            ]),
+          ),
+        ),
+      ),
+    );
   }
 }
